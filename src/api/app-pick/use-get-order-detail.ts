f84493c8df9014c1819ddd10db283cb1ad692f56
Cao Hoàng <caohoang2911@gmail.com>
2024-09-09 23:03:10 +0700

    update api order

--- src/api/app-pick/use-get-order-detail.ts
+++ src/api/app-pick/use-get-order-detail.ts
@@ -14,21 +14,10 @@ const getDetailOrder = async ({ orderCode }: Variables): Promise<Response> => {
   const params = {
     orderCode,
   };
-  return await axiosClient.get(`order/getOrderDetail`, { params });
+  return await axiosClient.get(`app-pick/getOrderDetail`, { params });
 };
 
 export const SHARE_SECRET_KEY = 'uifTjoY24DRGWz3NjcVa7w';
-
-const getDetailShareOrder = async ({
-  orderCode,
-}: Variables): Promise<Response> => {
-  const params = {
-    orderCode,
-    secretKey: SHARE_SECRET_KEY,
-  };
-
-  return await axiosClient.get(`share/order`, { params });
-};
 
 export const useOrderDetailQuery = ({ orderCode }: Variables) => {
   return useQuery({
@@ -37,12 +26,5 @@ export const useOrderDetailQuery = ({ orderCode }: Variables) => {
       return getDetailOrder({ orderCode });
     },
     enabled: true,
-  });
-};
-
-export const useOrderShareDetailQuery = ({ orderCode }: Variables) => {
-  return useQuery({
-    queryKey: ['useOrderShareDetailQuery'],
-    queryFn: () => getDetailShareOrder({ orderCode }),
   });
 };
