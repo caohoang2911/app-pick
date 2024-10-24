46b63dfaf5ce1bbe061884333c28f165052c03c5
Cao Hoàng <caohoang2911@gmail.com>
2024-10-24 21:32:02 +0700

    update headre

--- src/api/shared/client.tsx
+++ src/api/shared/client.tsx
@@ -5,10 +5,9 @@ import { signOut } from '~/src/core';
 import { getToken } from '~/src/core/store/auth/utils';
 
 export const axiosClient = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  baseURL: "https://oms-api-dev.seedcom.vn/",
   headers: {
     accept: 'application/json',
-    // zas: 'aK0eXSFmvO8GRnG_RSKFNi3HqY6bFZfNHKesulQTHh7OFkhSNj_tS55T4V22jGCq5ee5tZcs0cHMewGGu57O7wSTnGsUPtCt7tpja0rId5hrWvKzSc8ITUl1TSc9R3QySsOiQPV0qBqg6jBDGkcpFYfFnGsrNY5_fuy6ysa_bRzBndei0BL8wzPB2HlPbpzrSqS3ydy4o6TCNDxr1oL3ZhWJV6tZOFi-q1CqDjSv4bg',
   },
 });
 
@@ -53,11 +52,11 @@ axiosClient.interceptors.response.use(function (
 axiosClient.interceptors.request.use(function (config: any) {
   const token = getToken();
   
+  console.log('config', config.baseURL);
+  
   if (token) {
     config.headers.zas = token;
   }
-
-  config.baseURL = process.env.EXPO_PUBLIC_API_URL
 
   if (config.data instanceof FormData) {
     config.headers['Content-Type'] = 'multipart/form-data';
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -153,7 +153,7 @@ function Providers({ children }: { children: React.ReactNode }) {
                   {loading && <Loading />}
                   <View className="absolute bottom-3 right-5 z-10">
                     <Text className="text-gray-500 text-xs">
-                      {`Development ${VERSION}`}
+                      {`${process.env.EXPO_PUBLIC_API_URL}`}
                     </Text>
                   </View>
                   {children}
