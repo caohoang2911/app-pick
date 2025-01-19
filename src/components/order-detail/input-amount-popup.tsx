c7cd647fd2885e02ea6fedd2b1a173202eea286d
Cao Hoàng <caohoang2911@gmail.com>
2025-01-20 00:05:29 +0700

    pharse 2

--- src/components/order-detail/input-amount-popup.tsx
+++ src/components/order-detail/input-amount-popup.tsx
@@ -62,7 +62,7 @@ const InputAmountPopup = ({}) => {
   const renderTitle = useMemo(() => {
     return (
       <View className="flex justify-between gap-1">
-        <Text className="font-semibold">{productName}</Text>
+        <Text numberOfLines={1} className="font-semibold">{productName}</Text>
         <Badge className="self-start" label={`SL đặt: ${currentProduct?.quantity} ${currentProduct?.unit || ""}`}></Badge>
       </View>
     )
@@ -87,155 +87,156 @@ const InputAmountPopup = ({}) => {
       
       setOrderTemToPicked({ pickedItem, orderCode: code});
       
-      setOrderPickProduct(pickedItem);
-      setCurrentPid(null);
-      toggleShowAmountInput(false);
       resetForm();
-      setQuantityFromBarcode(0);
+
+      setOrderPickProduct(pickedItem);
+      reset();
   }
 
+  const reset = () => {
+    setCurrentPid(null);
+    toggleShowAmountInput(false);
+    setQuantityFromBarcode(0);
+  }
 
   return (
-    <SBottomSheet
-      renderTitle={renderTitle}
-      ref={inputBottomSheetRef}
-      visible={isShowAmountInput}
-      onClose={() => {
-        toggleShowAmountInput(false);
+    <Formik
+      initialValues={{
+        pickedQuantity: displayPickedQuantity, 
+        pickedError: (currentProduct as any)?.pickedError || '', 
+        pickedNote: (currentProduct as any)?.pickedNote || ''
       }}
-    > 
-      <View className="flex-1 px-4 mt-4 pb-4 gap-4">
-        <Formik
-          initialValues={{
-            pickedQuantity: displayPickedQuantity, 
-            pickedError: (currentProduct as any)?.pickedError || '', 
-            pickedNote: (currentProduct as any)?.pickedNote || ''
+      validateOnChange
+      onSubmit={handleSubmit}
+    >
+    {({ values, handleBlur, setFieldValue, handleSubmit, setErrors }) => {
+      const isError = values?.pickedQuantity < quantity && !values?.pickedError;
+      useEffect(() => {
+        if(quantityFromBarcode) {
+          setFieldValue('pickedQuantity', quantityFromBarcode.toString());
+        }
+      }, [quantityFromBarcode]);
+
+      return (
+        <SBottomSheet
+          renderTitle={renderTitle}
+          ref={inputBottomSheetRef}
+          visible={isShowAmountInput}
+          onClose={() => {
+            reset();
           }}
-          validateOnChange
-          onSubmit={handleSubmit}
-        >
-          {({ values, errors, handleBlur, setFieldValue, handleSubmit, setErrors }) => {
-
-            const isError = values?.pickedQuantity < quantity && !values?.pickedError;
-            useEffect(() => {
-              if(quantityFromBarcode) {
-                setFieldValue('pickedQuantity', quantityFromBarcode.toString());
-              }
-            }, [quantityFromBarcode]);
-
-            return (
-              <>
-                <View className="flex flex-row gap-2 items-center" style={{ position: 'relative' }}>
-                  <View className="flex-1" style={{ marginRight: 113}}>
-                    <Input
-                      selectTextOnFocus
-                      labelClasses="font-medium"
-                      label="Số lượng pick"
-                      placeholder="Nhập số lượng"
-                      inputClasses="text-center"
-                      keyboardType="decimal-pad"
-                      onChangeText={(value: string) => {
-                        setFieldValue('pickedQuantity', formatDecimal(value));
+        > 
+          <View className="flex-1 px-4 mt-4 pb-4 gap-4">
+            <View className="flex flex-row gap-2 items-center" style={{ position: 'relative' }}>
+              <View className="flex-1" style={{ marginRight: 113}}>
+                <Input
+                  selectTextOnFocus
+                  labelClasses="font-medium"
+                  label="Số lượng pick"
+                  placeholder="Nhập số lượng"
+                  inputClasses="text-center"
+                  keyboardType="decimal-pad"
+                  onChangeText={(value: string) => {
+                    setFieldValue('pickedQuantity', formatDecimal(value));
+                    if(Number(values?.pickedQuantity) >= Number(quantity)) {
+                      setFieldValue('pickedError', null);
+                    }
+                  }}
+                  editable={!isCampaign}
+                  useBottomSheetTextInput
+                  error={(Number(values?.pickedQuantity) < quantity && !values?.pickedError) && "Số lượng pick nhỏ hơn số lượng đặt. Vui lòng chọn lý do"}
+                  name="pickedQuantity"
+                  value={values?.pickedQuantity.toString()}
+                  onBlur={handleBlur('pickedQuantity')}
+                  defaultValue="0"
+                  prefix={
+                    <TouchableOpacity
+                      disabled={isCampaign}
+                      onPress={() => {
+                        const valueChange = roundToDecimalDecrease(Number(values?.pickedQuantity || 0))
+                        if (Number(valueChange) < 0) {
+                          setFieldValue('pickedQuantity', 0);
+                          return;
+                        };
+                        setFieldValue('pickedQuantity', Number(valueChange));
                         if(Number(values?.pickedQuantity) >= Number(quantity)) {
                           setFieldValue('pickedError', null);
                         }
                       }}
-                      editable={!isCampaign}
-                      useBottomSheetTextInput
-                      error={(Number(values?.pickedQuantity) < quantity && !values?.pickedError) && "Số lượng pick nhỏ hơn số lượng đặt. Vui lòng chọn lý do"}
-                      name="pickedQuantity"
-                      value={values?.pickedQuantity.toString()}
-                      onBlur={handleBlur('pickedQuantity')}
-                      defaultValue="0"
-                      prefix={
-                        <TouchableOpacity
-                          disabled={isCampaign}
-                          onPress={() => {
-                            const valueChange = roundToDecimalDecrease(Number(values?.pickedQuantity || 0))
-                            if (Number(valueChange) < 0) {
-                              setFieldValue('pickedQuantity', 0);
-                              return;
-                            };
-                            setFieldValue('pickedQuantity', Number(valueChange));
-                            if(Number(values?.pickedQuantity) >= Number(quantity)) {
-                              setFieldValue('pickedError', null);
-                            }
-                          }}
-                        >
-                          <View className="size-8 rounded-full bg-gray-200">
-                            <View className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                              <Text className="text-2xl w-full h-full text-center text-blue-500">
-                                -
-                              </Text>
-                            </View>
-                          </View>
-                        </TouchableOpacity>
-                      }
-                      suffix={
-                        <TouchableOpacity
-                          disabled={isCampaign}
-                          onPress={() => {
-                            const valueChange = roundToDecimalIncrease(Number(values?.pickedQuantity || 0))
-                            if (Number(valueChange) < 0) {
-                              setFieldValue('pickedQuantity', 0);
-                              return;
-                            };
-                            setFieldValue('pickedQuantity', Number(valueChange));
-
-                            if(Number(values?.pickedQuantity) >= Number(quantity)) {
-                              setFieldValue('pickedError', null);
-                            }
-                          }}
-                        >
-                          <View className="size-8 rounded-full bg-gray-200">
-                            <View className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                              <Text className="text-2xl w-full h-full text-center text-blue-500">
-                                +
-                              </Text>
-                            </View>
-                          </View>
-                        </TouchableOpacity>
-                      }
-                    />
-                  </View>
-                  <View style={{ position: 'absolute', top: Platform.OS === 'ios' ? 32 : 38, right: 0 }}>
-                    <View className="flex flex-row items-center gap-2">
-                      <View className="bg-gray-200 rounded-lg p-2" style={{ width: 70 }}>
-                        <Text className="text-gray-300 text-ellipsis text-center text-xs font-medium">{currentProduct?.unit}</Text>
+                    >
+                      <View className="size-8 rounded-full bg-gray-200">
+                        <View className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                          <Text className="text-2xl w-full h-full text-center text-blue-500">
+                            -
+                          </Text>
+                        </View>
                       </View>
-                      <TouchableOpacity onPress={() => {
-                          toggleScanQrCodeProduct(true, { isNewScan: false });
-                        }}>
-                          <View className=" bg-colorPrimary rounded-md size-8 flex flex-row justify-center items-center">
-                            <FontAwesome name="qrcode" size={18} color="white" />
-                          </View>
-                      </TouchableOpacity>
-                    </View>
-                  </View>
-                </View>
-                <SDropdown
-                  data={productPickedErrors}
-                  label="Chọn lý do"
-                  labelClasses="font-medium"
-                  dropdownPosition="top"
-                  placeholder="Vui lòng chọn"
-                  allowClear={true}
-                  disabled={Number(values?.pickedQuantity) >= Number(quantity)}
-                  value={values?.pickedError}
-                  onSelect={(value: string) => {
-                    setFieldValue('pickedError', formatDecimal(value));
-                    setErrors({})
-                  }}
-                  onClear={() => {
-                    setFieldValue('pickedError', '');
-                  }}
+                    </TouchableOpacity>
+                  }
+                  suffix={
+                    <TouchableOpacity
+                      disabled={isCampaign}
+                      onPress={() => {
+                        const valueChange = roundToDecimalIncrease(Number(values?.pickedQuantity || 0))
+                        if (Number(valueChange) < 0) {
+                          setFieldValue('pickedQuantity', 0);
+                          return;
+                        };
+                        setFieldValue('pickedQuantity', Number(valueChange));
+
+                        if(Number(values?.pickedQuantity) >= Number(quantity)) {
+                          setFieldValue('pickedError', null);
+                        }
+                      }}
+                    >
+                      <View className="size-8 rounded-full bg-gray-200">
+                        <View className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                          <Text className="text-2xl w-full h-full text-center text-blue-500">
+                            +
+                          </Text>
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  }
                 />
-                <Button onPress={handleSubmit as any} label={'Xác nhận'} disabled={isError} />
-              </>
-          )}}
-        </Formik>
-      </View>
-    </SBottomSheet>
+              </View>
+              <View style={{ position: 'absolute', top: Platform.OS === 'ios' ? 32 : 38, right: 0 }}>
+                <View className="flex flex-row items-center gap-2">
+                  <View className="bg-gray-200 rounded-lg p-2" style={{ width: 70 }}>
+                    <Text className="text-gray-300 text-ellipsis text-center text-xs font-medium">{currentProduct?.unit}</Text>
+                  </View>
+                  <TouchableOpacity onPress={() => {
+                      toggleScanQrCodeProduct(true, { isNewScan: false });
+                    }}>
+                      <View className=" bg-colorPrimary rounded-md size-8 flex flex-row justify-center items-center">
+                        <FontAwesome name="qrcode" size={18} color="white" />
+                      </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <SDropdown
+              data={productPickedErrors}
+              label="Chọn lý do"
+              labelClasses="font-medium"
+              dropdownPosition="top"
+              placeholder="Vui lòng chọn"
+              allowClear={true}
+              disabled={Number(values?.pickedQuantity) >= Number(quantity)}
+              value={values?.pickedError}
+              onSelect={(value: string) => {
+                setFieldValue('pickedError', formatDecimal(value));
+                setErrors({})
+              }}
+              onClear={() => {
+                setFieldValue('pickedError', '');
+              }}
+            />
+            <Button onPress={handleSubmit as any} label={'Xác nhận'} disabled={isError} />
+          </View>
+        </SBottomSheet>
+        )}}
+    </Formik>
   );
 };
 
