83f3cce61b5fe9ab8017f0e0d79a1b093fe38056
Cao Hoàng <caohoang2911@gmail.com>
2025-03-30 23:57:16 +0700

    update

--- src/components/order-invoice/header.tsx
+++ src/components/order-invoice/header.tsx
@@ -45,38 +45,14 @@ type Props = {
   onClickHeaderAction?: () => void;
 };
 
-const OrderPickHeader = ({ onClickHeaderAction }: Props) => {
-  const keyword = useOrderPick.use.keyword();
+const OrderPickHeader = () => {
   const { code } = useGlobalSearchParams<{ code: string }>();
-  const [value, setValue] = useState<string>();
-
-  useEffect(() => {
-    setValue(keyword);
-  }, [keyword]);
-
-  useEffect(() => {
-    setKeyword('');
-
-    return () => {
-      setKeyword('');
-    }
-  }, [code])
-
-  const handleSearch = useCallback(
-    debounce((value: string) => {
-      setKeyword(value);
-    }, 400),
-    []
-  );
-
   const orderDetail: OrderDetail = useOrderPick.use.orderDetail();
   const { header } = orderDetail;
-  const { status, statusName,  lastTimeUpdateStatus, tags, picker } = header || {};
-
-  const shouldDisplayQrScan = useCanEditOrderPick();
+  const { status, statusName,  lastTimeUpdateStatus, tags } = header || {};
 
   return (
-    <View className="px-4 bg-white">
+    <View className="px-4 bg-white pb-3">
       <View className="flex-row justify-between items-center">
         <View className="flex flex-row gap-2 justify-between flex-1 items-center">
           <View className='flex flex-row items-center gap-2'>
@@ -94,34 +70,6 @@ const OrderPickHeader = ({ onClickHeaderAction }: Props) => {
         <HeaderActionBtn />
       </View>
       <HeaderTags tags={tags} />
-      <View className="flex flex-row mt-2 justify-between items-center pb-3 gap-3">
-        <Input
-          className="flex-grow"
-          placeholder="SKU, tên sản phẩm"
-          prefix={<SearchLine width={20} height={20} />}
-          onChangeText={(value: string) => {
-            setValue(value);
-            handleSearch(value)
-          }}
-          value={value}
-          onClear={() => {
-            setValue("");
-            setKeyword('');
-          }}
-          allowClear
-        />
-        {shouldDisplayQrScan && (
-          <TouchableOpacity onPress={() => {
-            toggleScanQrCodeProduct(true);
-            setSuccessForBarcodeScan('');
-            setIsEditManual(false);
-          }}>
-            <View className=" bg-colorPrimary rounded-md size-10 flex flex-row justify-center items-center">
-              <FontAwesome name="qrcode" size={24} color="white" />
-            </View>
-          </TouchableOpacity>
-        )}
-      </View>
     </View>
   );
 };
