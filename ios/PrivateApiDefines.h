55eb9fe0b41ed33f0bea30e5f407e0b9d7196069
Cao Hoàng <caohoang2911@gmail.com>
2025-04-27 23:07:57 +0700

    update ios 18

---
+++ ios/PrivateApiDefines.h
@@ -0,0 +1,10 @@
+#ifndef PrivateApiDefines_h
+#define PrivateApiDefines_h
+
+// Định nghĩa các thay thế cho API riêng tư
+#define _isKeyDown isKeyDownReplacement
+#define _modifiedInput modifiedInputReplacement
+#define _modifierFlags modifierFlagsReplacement
+#define UIKeyboardImpl UIKeyboardImplReplacement
+
+#endif /* PrivateApiDefines_h */ 
