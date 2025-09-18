0f1278aa3882f4c5fe42fe9d220149d7326aaefe
Cao Hoàng <caohoang2911@gmail.com>
2025-09-18 22:34:31 +0700

    active user app

--- src/api/auth/use-authorize-user-password.ts
+++ src/api/auth/use-authorize-user-password.ts
@@ -1,6 +1,7 @@
 import { axiosClient } from '@/api/shared';
 import { useMutation } from '@tanstack/react-query';
 import { UserInfo } from '~/src/core/store/auth/utils';
+import { useSetAppVersionWithAutoInfo } from '../app-pick/use-set-app-version';
 
 const authorizeUserPassword = async (params: Variables): Promise<Response> => {
   return await axiosClient.post('auth/authorizeUserPassword', params);
@@ -10,9 +11,13 @@ type Variables = { username: string; password: string };
 
 type Response = { error: string } & { data: UserInfo }
 
-export const useAuthorizeUserPassword = (cb?: (data: Response) => void) => useMutation({
+export const useAuthorizeUserPassword = (cb?: (data: Response) => void) => {
+  const { setVersionWithAutoInfo } = useSetAppVersionWithAutoInfo();
+  return useMutation({
   mutationFn: (params: Variables) => authorizeUserPassword(params),
   onSuccess: (data: Response) => {
+    setVersionWithAutoInfo();
     cb?.(data);
   },
 });
+};
