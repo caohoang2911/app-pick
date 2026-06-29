275e2b3c72a957cf0f1a09863a5d32f1caf75a49
Cao Hoàng <caohoang2911@gmail.com>
2026-06-29 23:28:26 +0700

    fix search dropdown

--- src/components/SearchableDropdown.tsx
+++ src/components/SearchableDropdown.tsx
@@ -296,7 +296,7 @@ const styles = StyleSheet.create({
 
   rowContainer: {
     flexDirection: 'row',
-    alignItems: 'flex-start',
+    alignItems: 'center',
     width: '100%',
     zIndex: 1000,
   },
